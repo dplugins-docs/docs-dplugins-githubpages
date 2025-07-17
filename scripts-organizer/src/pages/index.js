13d9c2e62762a3369d8241f559f677d1657fc403
Marko Krstic <mailme@markokrstic.com>
2025-07-17 12:14:30 +0200

    ...

--- scripts-organizer/src/pages/index.js
+++ scripts-organizer/src/pages/index.js
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import SearchBar from '@theme/SearchBar';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
@@ -13,15 +14,18 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          The most advanded WordPress <br />  Code snippet plugin - {siteConfig.title} 
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <p className="hero__subtitle">Create fully customizable code snippets for your posts, user profiles, settings pages, and more.</p>
+        <div style={{marginBottom: '2rem'}} className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Get started
           </Link>
+        </div>
+        <div className={styles.searchContainer}>
+          <SearchBar />
         </div>
       </div>
     </header>
